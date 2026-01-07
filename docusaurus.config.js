da6b67e59f176a4644249544076ad25fc3bdbcb0
TasnimChowdhury171 <fortestingtas@gmail.com>
2026-01-07 12:36:22 +0600

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,63 +3,13 @@ const config = {
   tagline: 'Complete guide to using our system',
   favicon: 'img/favicon.ico',
 
+  // এই lines change করুন:
   url: 'https://tasnimchowdhury171.github.io',
   baseUrl: '/user-manual/',
-  
   trailingSlash: true,
 
   organizationName: 'TasnimChowdhury171',
   projectName: 'user-manual',
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-        },
-        blog: {
-          showReadingTime: true,
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'User Manual',
-        logo: {
-          alt: 'Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Documentation',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} User Manual.`,
-      },
-    }),
+  
+  // ... rest of code
 };
-
-module.exports = config;
