332a6f37a514018a958e5b5b6e6750f0602271e8
TasnimChowdhury171 <fortestingtas@gmail.com>
2026-01-07 13:58:51 +0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,15 +1,74 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'User Manual',
   tagline: 'Complete guide to using our system',
   favicon: 'img/favicon.ico',
 
-  // এই lines change করুন:
   url: 'https://tasnimchowdhury171.github.io',
   baseUrl: '/user-manual/',
+organizationName: 'TasnimChowdhury171',
+projectName: 'user-manual',
+
   trailingSlash: true,
 
   organizationName: 'TasnimChowdhury171',
   projectName: 'user-manual',
-  
-  // ... rest of code
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'User Manual',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} User Manual.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
 };
+
+export default config;
